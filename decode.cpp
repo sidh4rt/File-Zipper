e9bd928c81333a1bda018af51957a85237e1cf90
Siddharth <sid.jangpangi@gmail.com>
2022-09-24 15:43:14 +0530

    initial commit

---
+++ decode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "huff.hpp"
+using namespace std;
+
+int main (int argc, char* argv[]) {
+    if (argc != 3) {
+        cout << "Failed to detect Files..";
+        exit(1);
+    }
+    huff f (argv[1], argv[2]);
+    f.unzip();
+    cout << "Decompressed Successfully!" << endl;
+    return 0;
+}
